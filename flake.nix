01b66c9865afb34e406c4dac746c2e04e54e1cce
Christian Albrecht <christian.albrecht@mayflower.de>
2026-04-18 14:30:18 +0200

    disable nodePackages for sorting out later

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
         final: prev:
         foldl' (flip extends) (_: prev) [
           inputs.emacs-overlay.overlay
-          inputs.nodejs.overlay
+          #inputs.nodejs.overlay
           inputs.rust.overlay
           top.config.flake.overlays.emacs
         ] final;
@@ -71,22 +71,22 @@
             }
           );
 
-          emacsNodePackages = attrValues {
-            inherit (if useLatestNodeJS then prev.nodePackages_latest else prev.nodePackages)
-              eslint
-              eslint_d
-              #import-js
-              jsonlint
-              prettier
-              standardx
-              tslint
-              trepan-ni
-              typescript
-              typescript-language-server
-              bash-language-server # intelephense
-              yaml-language-server
-              ;
-          };
+          #emacsNodePackages = attrValues {
+          #  inherit (if useLatestNodeJS then prev.nodePackages_latest else prev.nodePackages)
+          #    eslint
+          #    eslint_d
+          #    #import-js
+          #    jsonlint
+          #    prettier
+          #    standardx
+          #    tslint
+          #    trepan-ni
+          #    typescript
+          #    typescript-language-server
+          #    bash-language-server # intelephense
+          #    yaml-language-server
+          #    ;
+          #};
 
           emacsExtraPathPackages = with final; [
             #TODO crate2nix
@@ -275,8 +275,8 @@
                   --set LIBCLANG_PATH "${libclangLib}" \
                   --set BINDGEN_EXTRA_CLANG_ARGS "-isystem ${libclangIncludes}" \
                   --set RUST_SRC_PATH "${rustStable.rust-src}/lib/rustlib/src/rust/library" \
-                  --prefix PERL5LIB : "$out/lib/perl5/site_perl/5.34.0:$out/lib/perl5/site_perl/5.34.0/x86_64-linux-thread-multi" \
-                  --prefix PATH : $out/bin:${makeBinPath emacsNodePackages}
+                  --prefix PERL5LIB : "$out/lib/perl5/site_perl/5.34.0:$out/lib/perl5/site_perl/5.34.0/x86_64-linux-thread-multi" #\
+                  #--prefix PATH : $out/bin:''${makeBinPath emacsNodePackages}
               '';
             };
 
